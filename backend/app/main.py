95edab85f715ffd0ebe0b3e9354004d5d254178e
royisice <rocexyz@gmail.com>
2026-01-31 20:08:46 +0800

    Add risk endpoint (mock) for flood and heat monitoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,16 @@
 from fastapi import FastAPI
-from backend.app.api.routes import router as api_router
+from app.api.routes import router as base_router
+from app.api.risk import router as risk_router
 
 app = FastAPI(
-    title = "UrbanResQ API",
-    description = "Backend API for YDTC Flood & Extreme Heat Monitoring System",
-    version = "0.1.0"
+    title="UrbanResQ API",
+    description="Backend API for YDTC Flood & Extreme Heat Monitoring System",
+    version="0.1.0"
 )
 
-app.include_router(api_router, prefix ="/api")
+app.include_router(base_router, prefix="/api")
+app.include_router(risk_router, prefix="/api")
 
 @app.get("/")
 def root():
-    return {
-      "status" : "running",
-      "service" : "UrbanResQ Backend"
-    }
+    return {"status": "running", "service": "UrbanResQ Backend"}
